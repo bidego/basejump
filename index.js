7531e5b499c28bd5f230c6acfb50924f9939acbd
Gonzalo <gonzalo.bide@gmail.com>
2017-10-16 14:13:35 -0300

    app.get modifyed

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
+/*
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+*/
+app.get('/', function(req, res) {
+  var fileName = path.join(__dirname, 'index.html');
+  res.sendFile(fileName, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
