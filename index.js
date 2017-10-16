d4bed44b56ad838fd2de4388f6f6b15cfcd1939f
Gonzalo <gonzalo.bide@gmail.com>
2017-10-16 14:19:48 -0300

    modifyed view/pages/index.ejs. indexJs 2 app.get basejump imported, commented

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-/*
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-*/
+/*
 app.get('/', function(req, res) {
   var fileName = path.join(__dirname, 'index.html');
   res.sendFile(fileName, function (err) {
@@ -25,7 +25,30 @@ app.get('/', function(req, res) {
     }
   });
 });
+app.get('/:datestring', function(req,res) {
+  var myDate;
+  if(/^\d{8,}$/.test(req.params.datestring)) {
+    myDate = moment(req.params.datestring, "X");
+  } else {
+    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+  }
 
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+
+
+});
+
+*/
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
